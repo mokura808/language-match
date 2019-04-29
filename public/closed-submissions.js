e45b6d0c42ae7114dfb4e5c414047703a34143c1
mokura808 <49482585+mokura808@users.noreply.github.com>
2019-04-29 19:36:16 -0400

    Add files via upload

---
+++ public/closed-submissions.js
@@ -0,0 +1,6 @@
+var homeButton = document.getElementById("home-button");
+homeButton.addEventListener("click", function() {
+    window.location.href = "/home";
+	}
+
+);
---
+++ public/match-not-ready.js
@@ -0,0 +1,6 @@
+var logoutButton = document.getElementById("log-out-button");
+logoutButton.addEventListener("click", function() {
+   	 window.location.href = "/user-login";
+	}
+
+);
